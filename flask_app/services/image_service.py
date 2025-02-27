08153db2dca3b84ded059b16d0a56d0fb13ead11
felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2025-02-27 15:01:58 -0300

    OCR cpf corrigido. Emplacamento com problemas.

--- flask_app/services/image_service.py
+++ flask_app/services/image_service.py
@@ -20,29 +20,11 @@ async def processar_imagem_com_ocr(update: Update, context: CallbackContext):
     try:
         # Abre a imagem em uma thread separada
         img = await asyncio.to_thread(Image.open, file_path)
+        texto = await asyncio.to_thread(pytesseract.image_to_string, img)
         
-        # Pré-processamento da imagem:
-        # Converte para escala de cinza
-        img = await asyncio.to_thread(ImageOps.grayscale, img)
-        # Aumenta o contraste
-        enhancer = ImageEnhance.Contrast(img)
-        img = await asyncio.to_thread(enhancer.enhance, 2)
-        # Aplica filtro de nitidez
-        img = await asyncio.to_thread(img.filter, ImageFilter.SHARPEN)
-        # Redimensiona a imagem (aumenta 150% do tamanho)
-        novo_tamanho = (int(img.width * 1.5), int(img.height * 1.5))
-        img = await asyncio.to_thread(img.resize, novo_tamanho)
-        # Aplica threshold para binarização (ajuste o valor se necessário)
-        threshold = 140
-        img = await asyncio.to_thread(lambda im: im.point(lambda p: 255 if p > threshold else 0), img)
-        
-        # Realiza OCR usando Tesseract com configuração customizada
-        custom_config = r'--oem 3 --psm 6'
-        texto = await asyncio.to_thread(pytesseract.image_to_string, img, config=custom_config)
-        print("OCR Result:", texto)
-        
+
         # Regex para identificar CPF e placa:
-        padrao_cpf = r"(\d{3}[\.]?\d{3}[\.]?\d{3}[-]?\d{2})"
+        padrao_cpf = r"\d{3}\.\d{3}\.\d{3}-\d{2}"
         padrao_placa = r"([A-Z]{3}\d{4}|[A-Z]{3}\d[A-Z]\d{2})"
         match_cpf = re.search(padrao_cpf, texto)
         match_placa = re.search(padrao_placa, texto, re.IGNORECASE)
