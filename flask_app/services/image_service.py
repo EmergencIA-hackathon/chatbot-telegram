e5dd0af35bf1cc14cfa186544e121f2ed92cd052
felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2025-03-03 13:48:37 -0300

    adicionando dados das imagens a ocorrencia no telegram

--- flask_app/services/image_service.py
+++ flask_app/services/image_service.py
@@ -1,10 +1,11 @@
 from PIL import Image, ImageOps, ImageEnhance, ImageFilter
 import pytesseract
-from telegram import Update
-from telegram.ext import CallbackContext
 import asyncio
 import re
 import os
+from session import usuario_ocorrencias
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import CallbackContext
 
 async def handle_image(update: Update, context: CallbackContext):
     await processar_imagem_com_ocr(update, context)
@@ -33,9 +34,51 @@ async def processar_imagem_com_ocr(update: Update, context: CallbackContext):
     
     except Exception as e:
         resposta = f"Erro ao processar a imagem: {str(e)}"
-    
     finally:
         if os.path.exists(file_path):
             await asyncio.to_thread(os.remove, file_path)
     
-    await context.bot.send_message(chat_id=update.message.chat_id, text=resposta)
+    
+    user_id = update.message.from_user.id
+    if user_id in usuario_ocorrencias:
+            estado = usuario_ocorrencias[user_id]["estado"]
+            
+            if estado == 'registrando_ocorrencia':
+                usuario_ocorrencias[user_id]["ocorrencia"] += f'{resposta}'
+                
+                teclado = InlineKeyboardMarkup([
+                    [
+                        InlineKeyboardButton(text="Sim", callback_data="sim_ocorrencia"),
+                        InlineKeyboardButton(text="Não", callback_data="nao_ocorrencia")
+                    ]
+                ])
+                await update.message.reply_text(
+                    f"📝 Resumo da ocorrência:\n\n"
+                    f"{usuario_ocorrencias[user_id]['ocorrencia']}\n\n"
+                    "Deseja adicionar mais alguma coisa? (sim/não)",
+                    reply_markup=teclado
+                )
+                usuario_ocorrencias[user_id]["estado"] = 'esperando_confirmacao'
+
+    else:
+            usuario_ocorrencias[user_id] = {
+                "estado": "registrando_ocorrencia",
+                "ocorrencia": f"Imagens: {resposta})"
+            }
+            
+            teclado = InlineKeyboardMarkup([
+                [
+                    InlineKeyboardButton(text="Sim", callback_data="sim_ocorrencia"),
+                    InlineKeyboardButton(text="Não", callback_data="nao_ocorrencia")
+                ]
+            ])
+            await update.message.reply_text(
+                f"📝 Resumo da ocorrência:\n\n"
+                f"{usuario_ocorrencias[user_id]['ocorrencia']}\n\n"
+                "Deseja registrar mais alguma coisa? (sim/não)",
+                reply_markup=teclado
+            )
+            usuario_ocorrencias[user_id]["estado"] = 'esperando_confirmacao'
+
+
+ 
